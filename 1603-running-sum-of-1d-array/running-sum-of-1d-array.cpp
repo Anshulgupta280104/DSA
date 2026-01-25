8a9c04587b55cc672a7468d8fd7a1b1aae05b194
Anshul Gupta <167673087+Anshulgupta280104@users.noreply.github.com>
2026-01-25 16:48:13 +0530

    Time: 0 ms (100.00%) | Memory: 12.6 MB (50.80%) - LeetSync

--- 1603-running-sum-of-1d-array/running-sum-of-1d-array.cpp
+++ 1603-running-sum-of-1d-array/running-sum-of-1d-array.cpp
@@ -1,20 +1,10 @@
 class Solution {
 public:
-    
-    void helper(vector<int>& nums, int i) {
-        if(i == nums.size())   // base case
-            return;
-        
-        nums[i] += nums[i-1]; // running sum
-        
-        helper(nums, i+1);    // recursive call
-    }
-    
     vector<int> runningSum(vector<int>& nums) {
-        if(nums.size() <= 1)
-            return nums;
         
-        helper(nums, 1);  // start from index 1
+        for(int i = 1; i < nums.size(); i++){
+            nums[i] += nums[i-1];
+        }
         
         return nums;
     }
