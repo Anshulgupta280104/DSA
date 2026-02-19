11619297e8f53a172780849c97eb2a020c13604e
Anshul Gupta <167673087+Anshulgupta280104@users.noreply.github.com>
2026-02-19 19:45:45 +0530

    Time: 43 ms (84.44%) | Memory: 28.9 MB (90.58%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -1,45 +1,52 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
+        
         vector<vector<int>> result;
         int n = nums.size();
-        
+
+        // Step 1: Sort the array
         sort(nums.begin(), nums.end());
-        
-        for (int i = 0; i < n - 2; i++) {
-            // Skip duplicate values for i
-            if (i > 0 && nums[i] == nums[i - 1]) 
+
+        // Step 2: Fix one element and use two pointers
+        for(int i = 0; i < n - 2; i++)
+        {
+            // Skip duplicate elements
+            if(i > 0 && nums[i] == nums[i-1])
                 continue;
-            
-            int left = i + 1;
-            int right = n - 1;
-            
-            while (left < right) {
-                int sum = nums[i] + nums[left] + nums[right];
-                
-                if (sum == 0) {
-                    result.push_back({nums[i], nums[left], nums[right]});
-                    
-                    // Skip duplicates for left
-                    while (left < right && nums[left] == nums[left + 1])
-                        left++;
-                    
-                    // Skip duplicates for right
-                    while (left < right && nums[right] == nums[right - 1])
-                        right--;
-                    
-                    left++;
-                    right--;
+
+            int start = i + 1;
+            int end = n - 1;
+
+            while(start < end)
+            {
+                int sum = nums[i] + nums[start] + nums[end];
+
+                if(sum == 0)
+                {
+                    result.push_back({nums[i], nums[start], nums[end]});
+
+                    start++;
+                    end--;
+
+                    // Skip duplicates
+                    while(start < end && nums[start] == nums[start-1])
+                        start++;
+
+                    while(start < end && nums[end] == nums[end+1])
+                        end--;
                 }
-                else if (sum < 0) {
-                    left++;
+                else if(sum < 0)
+                {
+                    start++;
                 }
-                else {
-                    right--;
+                else
+                {
+                    end--;
                 }
             }
         }
-        
+
         return result;
     }
 };
