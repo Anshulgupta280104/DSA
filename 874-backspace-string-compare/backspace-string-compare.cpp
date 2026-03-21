c22b523b69254f92c3eb89bbaa2f52333262d2e9
Anshul Gupta <167673087+Anshulgupta280104@users.noreply.github.com>
2026-03-21 23:43:19 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (12.78%) - LeetSync

--- 874-backspace-string-compare/backspace-string-compare.cpp
+++ 874-backspace-string-compare/backspace-string-compare.cpp
@@ -1,12 +1,27 @@
 class Solution {
 public:
     string build(string s) {
-        string res = "";
+        stack<char> st;
+
         for (char c : s) {
-            if (c != '#') res.push_back(c);
-            else if (!res.empty()) res.pop_back();
+            if (c != '#') {
+                st.push(c);
+            } else {
+                if (!st.empty()) {
+                    st.pop();
+                }
+            }
         }
-        return res;
+
+        // Convert stack to string
+        string result = "";
+        while (!st.empty()) {
+            result += st.top();
+            st.pop();
+        }
+
+        reverse(result.begin(), result.end());
+        return result;
     }
 
     bool backspaceCompare(string s, string t) {
