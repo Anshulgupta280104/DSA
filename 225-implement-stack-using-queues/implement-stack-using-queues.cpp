d31626f01aa5bf3a49dfed8b53ae3e90bb83963d
Anshul Gupta <167673087+Anshulgupta280104@users.noreply.github.com>
2026-03-31 23:56:54 +0530

    Time: 0 ms (100.00%) | Memory: 9.4 MB (98.22%) - LeetSync

---
+++ 225-implement-stack-using-queues/implement-stack-using-queues.cpp
@@ -0,0 +1,36 @@
+#include <queue>
+using namespace std;
+
+class MyStack {
+public:
+    queue<int> q;
+
+    MyStack() {
+        
+    }
+    
+    void push(int x) {
+        q.push(x);
+        
+        // rotate queue to make last element at front
+        int size = q.size();
+        for(int i = 0; i < size - 1; i++) {
+            q.push(q.front());
+            q.pop();
+        }
+    }
+    
+    int pop() {
+        int val = q.front();
+        q.pop();
+        return val;
+    }
+    
+    int top() {
+        return q.front();
+    }
+    
+    bool empty() {
+        return q.empty();
+    }
+};
