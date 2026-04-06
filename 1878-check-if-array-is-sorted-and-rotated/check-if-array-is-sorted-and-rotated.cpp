5a4137467f143387030a0f495310642f84af64b2
Anshul Gupta <167673087+Anshulgupta280104@users.noreply.github.com>
2026-04-07 00:05:14 +0530

    Time: 0 ms (100.00%) | Memory: 11.2 MB (81.91%) - LeetSync

--- 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.cpp
+++ 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.cpp
@@ -4,15 +4,12 @@ public:
         int n = nums.size();
         int count = 0;
 
-        for (int i = 0; i < n; i++) {
-            // Check if current element is greater than the next element
-            // Use (i + 1) % n to handle the wrap-around back to index 0
-            if (nums[i] > nums[(i + 1) % n]) {
+        for(int i = 0; i < n; i++) {
+            if(nums[i] > nums[(i + 1) % n]) {
                 count++;
             }
         }
 
-        // A sorted and rotated array can have at most one "drop"
         return count <= 1;
     }
 };
